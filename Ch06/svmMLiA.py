46ce58c54f1c8410666b80c59c59372e774e011d
xingrui <xingrui05@gmail.com>
2017-09-23 10:43:47 +0800

    update svmMLiA.py : change mat to array. reason : dot(array([[1,2,3],[4,5,6],[7,8,9]]), array([1,2,3])) will auto change to mat([[1,2,3],[4,5,6],[7,8,9]]) * mat([[1],[2],[3]])

--- Ch06/svmMLiA.py
+++ Ch06/svmMLiA.py
@@ -7,39 +7,38 @@ from numpy import *
 from time import sleep
 import sys
 
-def preprocess(dataMatrix, labelMatrix):
-    m, n = shape(dataMatrix)
-    storeDataMat = mat(zeros((m, m)))
-    storeLabelMat = dot(labelMatrix, labelMatrix.T)
+def preprocess(dataArray, labelArray):
+    m, n = shape(dataArray)
+    storeDataArray = zeros((m, m))
+    storeLabelArray = dot(reshape(labelArray, (-1,1)), reshape(labelArray, (1,-1)))
     for i in xrange(0,m):
         for j in xrange(0,m):
-            storeDataMat[i,j] = vdot(dataMatrix.A[i], dataMatrix.A[j])
-    return multiply(storeDataMat, storeLabelMat)
+            storeDataArray[i,j] = vdot(dataArray[i], dataArray[j])
+    return multiply(storeDataArray, storeLabelArray)
 
 # goal : maximize this calculateValue result
 # under KKT conditions.
 # 1 / sqrt(wTw) means the min value of distance from super plane and points(support vector points)
 # it shows that the result is increasing, but the distance is not always increasing.
-def calculateValue(alphas, storeMat):
-    alphas = mat(alphas)
-    alphasMatrix = dot(alphas.T, alphas)
-    wTw = sum(multiply(alphasMatrix, storeMat))
+def calculateValue(alphas, storeArray):
+    alphasArray = dot(reshape(alphas, (-1,1)), reshape(alphas, (1,-1)))
+    wTw = sum(multiply(alphasArray, storeArray))
     return sum(alphas) - 0.5 * wTw, 1 / sqrt(wTw)
 
 # print value to stderr for differentiate from other logs.
-def traceLog(trace, alphas, storeMat, indent=0):
+def traceLog(trace, alphas, storeArray, indent=0):
     if not trace:
         return
-    print >>sys.stderr, ' ' * indent, 'calculateValue:', calculateValue(alphas, storeMat)
+    print >>sys.stderr, ' ' * indent, 'calculateValue:', calculateValue(alphas, storeArray)
 
 def loadDataSet(fileName):
-    dataMat = []; labelMat = []
+    dataArr = []; labelArr = []
     fr = open(fileName)
     for line in fr.readlines():
         lineArr = line.strip().split('\t')
-        dataMat.append([float(lineArr[0]), float(lineArr[1])])
-        labelMat.append(float(lineArr[2]))
-    return dataMat,labelMat
+        dataArr.append([float(lineArr[0]), float(lineArr[1])])
+        labelArr.append(float(lineArr[2]))
+    return dataArr,labelArr
 
 def selectJrand(i,m):
     j=i #we want to select any J not equal to i
@@ -55,47 +54,47 @@ def clipAlpha(aj,H,L):
     return aj
 
 def smoSimple(dataMatIn, classLabels, C, toler, maxIter, trace=False):
-    dataMatrix = mat(dataMatIn); labelMat = array(classLabels)
-    b = 0; m,n = shape(dataMatrix)
+    dataArray = array(dataMatIn); labelArray = array(classLabels)
+    b = 0; m,n = shape(dataArray)
     alphas = zeros(m)
-    storeMat = preprocess(dataMatrix, mat(labelMat).T)
+    storeArray = preprocess(dataArray, array(labelArray))
     iter = 0
     while (iter < maxIter):
         alphaPairsChanged = 0
         for i in range(m):
-            fXi = vdot(multiply(alphas,labelMat), (dataMatrix*dataMatrix[i].T).A[:,0]) + b
-            Ei = fXi - float(labelMat[i])#if checks if an example violates KKT conditions
-            if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and (alphas[i] > 0)):
+            fXi = vdot(multiply(alphas,labelArray), dot(dataArray,dataArray[i])) + b
+            Ei = fXi - float(labelArray[i])#if checks if an example violates KKT conditions
+            if ((labelArray[i]*Ei < -toler) and (alphas[i] < C)) or ((labelArray[i]*Ei > toler) and (alphas[i] > 0)):
                 j = selectJrand(i,m)
-                fXj = vdot(multiply(alphas,labelMat), (dataMatrix*dataMatrix[j].T).A[:,0]) + b
-                Ej = fXj - float(labelMat[j])
+                fXj = vdot(multiply(alphas,labelArray), dot(dataArray,dataArray[j])) + b
+                Ej = fXj - float(labelArray[j])
                 alphaIold = alphas[i]; alphaJold = alphas[j];
-                if (labelMat[i] != labelMat[j]):
+                if (labelArray[i] != labelArray[j]):
                     L = max(0, alphas[j] - alphas[i])
                     H = min(C, C + alphas[j] - alphas[i])
                 else:
                     L = max(0, alphas[j] + alphas[i] - C)
                     H = min(C, alphas[j] + alphas[i])
                 if L==H: print "L==H"; continue
-                eta = 2.0 * vdot(dataMatrix.A[i], dataMatrix.A[j]) - vdot(dataMatrix.A[i], dataMatrix.A[i]) - vdot(dataMatrix.A[j], dataMatrix.A[j])
+                eta = 2.0 * vdot(dataArray[i], dataArray[j]) - vdot(dataArray[i], dataArray[i]) - vdot(dataArray[j], dataArray[j])
                 if eta >= 0: print "eta>=0"; continue
-                alphas[j] -= labelMat[j]*(Ei - Ej)/eta
+                alphas[j] -= labelArray[j]*(Ei - Ej)/eta
                 alphas[j] = clipAlpha(alphas[j],H,L)
                 if (abs(alphas[j] - alphaJold) < 0.00001): print "j not moving enough"; continue
-                alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])#update i by the same amount as j
+                alphas[i] += labelArray[j]*labelArray[i]*(alphaJold - alphas[j])#update i by the same amount as j
                                                                         #the update is in the oppostie direction
-                b1 = b - Ei- labelMat[i]*(alphas[i]-alphaIold)*vdot(dataMatrix.A[i], dataMatrix.A[i]) - labelMat[j]*(alphas[j]-alphaJold)*vdot(dataMatrix.A[i], dataMatrix.A[j])
-                b2 = b - Ej- labelMat[i]*(alphas[i]-alphaIold)*vdot(dataMatrix.A[i], dataMatrix.A[j]) - labelMat[j]*(alphas[j]-alphaJold)*vdot(dataMatrix.A[j], dataMatrix.A[j])
+                b1 = b - Ei- labelArray[i]*(alphas[i]-alphaIold)*vdot(dataArray[i], dataArray[i]) - labelArray[j]*(alphas[j]-alphaJold)*vdot(dataArray[i], dataArray[j])
+                b2 = b - Ej- labelArray[i]*(alphas[i]-alphaIold)*vdot(dataArray[i], dataArray[j]) - labelArray[j]*(alphas[j]-alphaJold)*vdot(dataArray[j], dataArray[j])
                 if (0 < alphas[i]) and (C > alphas[i]): b = b1
                 elif (0 < alphas[j]) and (C > alphas[j]): b = b2
                 else: b = (b1 + b2)/2.0
                 alphaPairsChanged += 1
                 print "iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged)
-                traceLog(trace, alphas, storeMat)
+                traceLog(trace, alphas, storeArray)
         if (alphaPairsChanged == 0): iter += 1
         else: iter = 0
         print "iteration number: %d" % iter
-    traceLog(trace, alphas, storeMat, 4)
+    traceLog(trace, alphas, storeArray, 4)
     return b,alphas
 
 def kernelTrans(X, A, kTup): #calc the kernel or transform data to a higher dimensional space
@@ -114,7 +113,7 @@ def kernelTrans(X, A, kTup): #calc the kernel or transform data to a higher dime
 class optStruct:
     def __init__(self,dataMatIn, classLabels, C, toler, kTup):  # Initialize the structure with the parameters 
         self.X = dataMatIn
-        self.labelMat = classLabels
+        self.labelArray = classLabels
         self.C = C
         self.tol = toler
         self.m = shape(dataMatIn)[0]
@@ -126,8 +125,8 @@ class optStruct:
             self.K[:,i] = kernelTrans(self.X, self.X[i,:], kTup)
         
 def calcEk(oS, k):
-    fXk = vdot(multiply(oS.alphas,oS.labelMat), oS.K.A[:,k]) + oS.b
-    Ek = fXk - oS.labelMat[k]
+    fXk = vdot(multiply(oS.alphas,oS.labelArray), oS.K.A[:,k]) + oS.b
+    Ek = fXk - oS.labelArray[k]
     return Ek
         
 def selectJ(i, oS, Ei):         #this is the second choice -heurstic, and calcs Ej
@@ -153,10 +152,10 @@ def updateEk(oS, k):#after any alpha has changed update the new value in the cac
         
 def innerL(i, oS):
     Ei = calcEk(oS, i)
-    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+    if ((oS.labelArray[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelArray[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
         j,Ej = selectJ(i, oS, Ei) #this has been changed from selectJrand
         alphaIold = oS.alphas[i]; alphaJold = oS.alphas[j];
-        if (oS.labelMat[i] != oS.labelMat[j]):
+        if (oS.labelArray[i] != oS.labelArray[j]):
             L = max(0, oS.alphas[j] - oS.alphas[i])
             H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
         else:
@@ -165,14 +164,14 @@ def innerL(i, oS):
         if L==H: print "L==H"; return 0
         eta = 2.0 * oS.K[i,j] - oS.K[i,i] - oS.K[j,j] #changed for kernel
         if eta >= 0: print "eta>=0"; return 0
-        oS.alphas[j] -= oS.labelMat[j]*(Ei - Ej)/eta
+        oS.alphas[j] -= oS.labelArray[j]*(Ei - Ej)/eta
         oS.alphas[j] = clipAlpha(oS.alphas[j],H,L)
         updateEk(oS, j) #added this for the Ecache
         if (abs(oS.alphas[j] - alphaJold) < 0.00001): print "j not moving enough"; return 0
-        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])#update i by the same amount as j
+        oS.alphas[i] += oS.labelArray[j]*oS.labelArray[i]*(alphaJold - oS.alphas[j])#update i by the same amount as j
         updateEk(oS, i) #added this for the Ecache                    #the update is in the oppostie direction
-        b1 = oS.b - Ei- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.K[i,i] - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.K[i,j]
-        b2 = oS.b - Ej- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.K[i,j]- oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.K[j,j]
+        b1 = oS.b - Ei- oS.labelArray[i]*(oS.alphas[i]-alphaIold)*oS.K[i,i] - oS.labelArray[j]*(oS.alphas[j]-alphaJold)*oS.K[i,j]
+        b2 = oS.b - Ej- oS.labelArray[i]*(oS.alphas[i]-alphaIold)*oS.K[i,j]- oS.labelArray[j]*(oS.alphas[j]-alphaJold)*oS.K[j,j]
         if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
         elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
         else: oS.b = (b1 + b2)/2.0
@@ -181,7 +180,7 @@ def innerL(i, oS):
 
 def smoP(dataMatIn, classLabels, C, toler, maxIter,kTup=('lin', 0), trace=False):    #full Platt SMO
     oS = optStruct(mat(dataMatIn),array(classLabels),C,toler, kTup)
-    storeMat = preprocess(mat(dataMatIn), mat(classLabels).T)
+    storeArray = preprocess(array(dataMatIn), array(classLabels))
     iter = 0
     entireSet = True; alphaPairsChanged = 0
     while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
@@ -191,7 +190,7 @@ def smoP(dataMatIn, classLabels, C, toler, maxIter,kTup=('lin', 0), trace=False)
                 innerRes = innerL(i,oS)
                 alphaPairsChanged += innerRes
                 print "fullSet, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged)
-                if innerRes : traceLog(trace, oS.alphas, storeMat)
+                if innerRes : traceLog(trace, oS.alphas, storeArray)
             iter += 1
         else:#go over non-bound (railed) alphas
             nonBoundIs = nonzero((oS.alphas > 0) * (oS.alphas < C))[0]
@@ -199,12 +198,12 @@ def smoP(dataMatIn, classLabels, C, toler, maxIter,kTup=('lin', 0), trace=False)
                 innerRes = innerL(i,oS)
                 alphaPairsChanged += innerRes
                 print "non-bound, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged)
-                if innerRes : traceLog(trace, oS.alphas, storeMat)
+                if innerRes : traceLog(trace, oS.alphas, storeArray)
             iter += 1
         if entireSet: entireSet = False #toggle entire set loop
         elif (alphaPairsChanged == 0): entireSet = True  
         print "iteration number: %d" % iter
-    traceLog(trace, oS.alphas, storeMat, 4)
+    traceLog(trace, oS.alphas, storeArray, 4)
     return oS.b,oS.alphas
 
 def calcWs(alphas,dataArr,classLabels):
