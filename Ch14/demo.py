defd89b454bd5d66134b5f2221502ca6f2ba2f56
xingrui <xingrui05@gmail.com>
2017-09-23 20:45:16 +0800

    change mat to array.

--- Ch14/demo.py
+++ Ch14/demo.py
@@ -1,2 +1,8 @@
 import svdRec
+from numpy import *
 svdRec.imgCompress()
+myArray = array(svdRec.loadExData())
+myArray[0,1]=myArray[0,0]=myArray[1,0]=myArray[2,0]=4
+myArray[3,3]=2
+print myArray
+print svdRec.recommend(myArray, 2)
--- Ch14/svdRec.py
+++ Ch14/svdRec.py
@@ -37,35 +37,35 @@ def pearsSim(inA,inB):
     return 0.5+0.5*corrcoef(inA, inB, rowvar = 0)[0][1]
 
 def cosSim(inA,inB):
-    num = float(inA.T*inB)
+    num = vdot(inA,inB)
     denom = la.norm(inA)*la.norm(inB)
     return 0.5+0.5*(num/denom)
 
-def standEst(dataMat, user, simMeas, item):
-    n = shape(dataMat)[1]
+def standEst(dataArray, user, simMeas, item):
+    n = shape(dataArray)[1]
     simTotal = 0.0; ratSimTotal = 0.0
     for j in range(n):
-        userRating = dataMat[user,j]
+        userRating = dataArray[user,j]
         if userRating == 0: continue
-        overLap = nonzero(logical_and(dataMat.A[:,item]>0, \
-                                      dataMat.A[:,j]>0))[0]
+        overLap = nonzero(logical_and(dataArray[:,item]>0, \
+                                      dataArray[:,j]>0))[0]
         if len(overLap) == 0: similarity = 0
-        else: similarity = simMeas(dataMat[overLap,item], \
-                                   dataMat[overLap,j])
+        else: similarity = simMeas(dataArray[overLap,item], \
+                                   dataArray[overLap,j])
         print 'the %d and %d similarity is: %f' % (item, j, similarity)
         simTotal += similarity
         ratSimTotal += similarity * userRating
     if simTotal == 0: return 0
     else: return ratSimTotal/simTotal
     
-def svdEst(dataMat, user, simMeas, item):
-    n = shape(dataMat)[1]
+def svdEst(dataArray, user, simMeas, item):
+    n = shape(dataArray)[1]
     simTotal = 0.0; ratSimTotal = 0.0
-    U,Sigma,VT = la.svd(dataMat)
-    Sig4 = mat(diag(Sigma[:4]))
-    xformedItems = dataMat.T * U[:,:4] * Sig4.I  #create transformed items
+    U,Sigma,VT = la.svd(dataArray)
+    Sig4 = diag(Sigma[:4])
+    xformedItems = dot(dot(dataArray.T, U[:,:4]) * la.inv(Sig4))  #create transformed items
     for j in range(n):
-        userRating = dataMat[user,j]
+        userRating = dataArray[user,j]
         if userRating == 0 or j==item: continue
         similarity = simMeas(xformedItems[item].T,\
                              xformedItems[j].T)
@@ -75,19 +75,19 @@ def svdEst(dataMat, user, simMeas, item):
     if simTotal == 0: return 0
     else: return ratSimTotal/simTotal
 
-def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
-    unratedItems = nonzero(dataMat.A[user]==0)[0]#find unrated items 
+def recommend(dataArray, user, N=3, simMeas=cosSim, estMethod=standEst):
+    unratedItems = nonzero(dataArray[user]==0)[0]#find unrated items 
     if len(unratedItems) == 0: return 'you rated everything'
     itemScores = []
     for item in unratedItems:
-        estimatedScore = estMethod(dataMat, user, simMeas, item)
+        estimatedScore = estMethod(dataArray, user, simMeas, item)
         itemScores.append((item, estimatedScore))
     return sorted(itemScores, key=operator.itemgetter(1), reverse=True)[:N]
 
-def printMat(inMat, thresh=0.8):
+def printArray(inArray, thresh=0.8):
     for i in range(32):
         for k in range(32):
-            if float(inMat[i,k]) > thresh:
+            if float(inArray[i,k]) > thresh:
                 print 1,
             else: print 0,
         print ''
@@ -96,11 +96,11 @@ def imgCompress(numSV=3, thresh=0.8):
     myl = []
     for line in open('0_5.txt').readlines():
         myl.append(map(int,line.strip()))
-    myMat = mat(myl)
+    myArray = array(myl)
     print "****original matrix******"
-    printMat(myMat, thresh)
-    U,Sigma,VT = la.svd(myMat)
+    printArray(myArray, thresh)
+    U,Sigma,VT = la.svd(myArray)
     SigRecon = diag(Sigma[:numSV])
-    reconMat = U[:,:numSV]*SigRecon*VT[:numSV]
+    reconArray = dot(dot(U[:,:numSV],SigRecon), VT[:numSV])
     print "****reconstructed matrix using %d singular values******" % numSV
-    printMat(reconMat, thresh)
+    printArray(reconArray, thresh)
