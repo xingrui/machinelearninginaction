467004bddf7df0cc9a96cb924e3bcde8c26117b6
xingrui <xingrui05@gmail.com>
2017-09-25 10:57:26 +0800

    change code style.

--- Ch15/proximalSVM.py
+++ Ch15/proximalSVM.py
@@ -3,22 +3,22 @@ Created on Feb 25, 2011
 
 @author: Peter
 '''
-import numpy
+from numpy import *
 import base64
 import pickle
 
 def map(key, value):
    # input key= class for one training example, e.g. "-1.0"
    classes = [float(item) for item in key.split(",")]   # e.g. [-1.0]
-   D = numpy.diag(classes)
+   D = diag(classes)
  
    # input value = feature vector for one training example, e.g. "3.0, 7.0, 2.0"
    featurematrix = [float(item) for item in value.split(",")]
-   A = numpy.mat(featurematrix)
+   A = mat(featurematrix)
  
    # create matrix E and vector e
-   e = numpy.mat(numpy.ones(len(A)).reshape(len(A),1))
-   E = numpy.mat(numpy.append(A,-e,axis=1)) 
+   e = mat(ones(len(A)).reshape(len(A),1))
+   E = mat(append(A,-e,axis=1)) 
  
    # create a tuple with the values to be used by reducer
    # and encode it with base64 to avoid potential trouble with '\t' and '\n' used
@@ -39,7 +39,7 @@ def reduce(key, values, mu=0.1):
     ETE, ETDe = pickle.loads(base64.b64decode(value))
     if sumETE == None:
       # create the I/mu with correct dimensions
-      sumETE = numpy.mat(numpy.eye(ETE.shape[1])/mu)
+      sumETE = mat(eye(ETE.shape[1])/mu)
     sumETE += ETE
  
     if sumETDe == None:
