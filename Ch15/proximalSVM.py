5a44bd9d10d5262ce275194f800139b0969abb6d
xingrui <xingrui05@gmail.com>
2017-09-25 11:41:51 +0800

    change mat to array. (BUT I am not sure whether the proximalSVM.py is right or wrong!!!)

--- Ch15/proximalSVM.py
+++ Ch15/proximalSVM.py
@@ -10,20 +10,20 @@ import pickle
 def map(key, value):
     # input key= class for one training example, e.g. "-1.0"
     classes = [float(item) for item in key.split(",")]   # e.g. [-1.0]
-    D = diag(classes)
+    D = classes[0]
  
     # input value = feature vector for one training example, e.g. "3.0, 7.0, 2.0"
     featurematrix = [float(item) for item in value.split(",")]
-    A = mat(featurematrix)
+    A = array(featurematrix)
  
     # create matrix E and vector e
-    e = ones((shape(A)[0],1))
-    E = mat(append(A,-e,axis=1)) 
+    e = ones(1)
+    E = append(A,-e,axis=1)
  
     # create a tuple with the values to be used by reducer
     # and encode it with base64 to avoid potential trouble with '\t' and '\n' used
     # as default separators in Hadoop Streaming
-    producedvalue = base64.b64encode(pickle.dumps((dot(E.T, E), dot(dot(E.T, D), e))))    
+    producedvalue = base64.b64encode(pickle.dumps((outer(E, E), multiply(multiply(E, D), e))))    
  
     # note: a single constant key "producedkey" sends to only one reducer
     # somewhat "atypical" due to low degree of parallism on reducer side
@@ -39,7 +39,7 @@ def reduce(key, values, mu=0.1):
         ETE, ETDe = pickle.loads(base64.b64decode(value))
         if sumETE == None:
             # create the I/mu with correct dimensions
-            sumETE = mat(eye(ETE.shape[1])/mu)
+            sumETE = eye(ETE.shape[1])/mu
         sumETE += ETE
  
         if sumETDe == None:
